61049987eb4ea7397f3c083a3883eff3a9fc7801
HuangShuYuBok <97289630@qq.com>
2020-11-03 08:46:42 +0800

    this a test

---
+++ Config/Function.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * 打印输出
+ **/
+function d($d)
+{
+	echo "<pre>";
+	print_r($d);
+	echo "</pre>";
+}
+
+/*
+ * 将数组转成Json格式
+ * type array
+ * */
+function asJson($array)
+{
+    if(!is_array($array)){
+        return "错误：：这不是一个数组";
+    }
+    return json_encode($array);
+}
---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+include "./Config/Db.php";
+include "./Config/Function.php";
+$db = new Mysql();
+
+$list = $db->find("oscshop_access","id=8");
+d($list);
+echo asJson("this string");
